c08c8c824bc04e01c522c723ce8338abde430761
Gulnaz-Tabassum <47949849+Gulnaz-Tabassum@users.noreply.github.com>
2021-10-08 15:21:36 +0530

    Create csv_reader.py

---
+++ csv_reader.py
@@ -0,0 +1,21 @@
+#importing csv
+import csv
+#opening salary_data.csv from your local computer
+file = open("Salary_Data.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()import csv
+file = open("Salary_Data.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()
